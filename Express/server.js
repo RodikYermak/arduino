919296343197ca3e27292da381532061ed797c8c
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-15 16:52:00 -0700

    cleanup

--- Express/server.js
+++ Express/server.js
@@ -2,7 +2,7 @@ import http from 'node:http';
 
 const server = http.createServer((req, res) => {
     console.log('server accessed');
-    res.end('<html><body>Hello</body></html>');
+    res.end('<html><body>Hello Rodion</body></html>');
 });
 
 server.listen(8000, () => console.log('listening 8000'));
