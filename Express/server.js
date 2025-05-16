79396f082e8bc52d56841d2d6f6a3bc6ff1e836b
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-15 20:13:40 -0700

    addign cors

--- Express/server.js
+++ Express/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { apiRouter } from './routes/apiRoutes.js';
 
 const PORT = 8000;
 
 const app = express();
+
+app.use(cors());
 
 app.use('/api', apiRouter);
 
