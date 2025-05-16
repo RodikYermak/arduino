f46ce6665a58f007dda9f3d003d24eec6ffe0fd2
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-15 17:54:46 -0700

    serving data

--- Express/server.js
+++ Express/server.js
@@ -10,19 +10,21 @@
 // console.log('Hello');
 
 import express from 'express';
+import { startups } from './data/data.js';
 
 const PORT = 8000;
 
-const celebrity = {
-    type: 'action hero',
-    name: 'JSON Statham',
-};
+// const celebrity = {
+//     type: 'action hero',
+//     name: 'JSON Statham',
+// };
 
 const app = express();
 
 // console.log(app);
 app.get('/', (req, res) => {
-    res.json(celebrity);
+    // res.json(celebrity);
+    res.json(startups);
 });
 
 app.listen(PORT, () => console.log(`server connected to port ${PORT}`));
