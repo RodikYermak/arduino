f039e5363ed8dfd203669e1df009a7a6ed01be27
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-15 20:09:18 -0700

    route not found

--- Express/server.js
+++ Express/server.js
@@ -1,75 +1,14 @@
-// import http from 'node:http';
-
-// const server = http.createServer((req, res) => {
-//     console.log('server accessed');
-//     res.end('<html><body>Hello Rodion</body></html>');
-// });
-
-// server.listen(8000, () => console.log('listening 8000'));
-
-// console.log('Hello');
-
 import express from 'express';
-import { startups } from './data/data.js';
+import { apiRouter } from './routes/apiRoutes.js';
 
 const PORT = 8000;
 
 const app = express();
 
-app.get('/api', (req, res) => {
-    let filteredData = startups;
+app.use('/api', apiRouter);
 
-    const { industry, country, continent, is_seeking_funding, has_mvp } = req.query;
-
-    if (industry) {
-        filteredData = filteredData.filter(
-            (startup) => startup.industry.toLowerCase() === industry.toLowerCase()
-        );
-    }
-
-    if (country) {
-        filteredData = filteredData.filter(
-            (startup) => startup.country.toLowerCase() === country.toLowerCase()
-        );
-    }
-
-    if (continent) {
-        filteredData = filteredData.filter(
-            (startup) => startup.continent.toLowerCase() === continent.toLowerCase()
-        );
-    }
-
-    if (is_seeking_funding) {
-        filteredData = filteredData.filter(
-            (startup) => startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
-        );
-    }
-
-    if (has_mvp) {
-        filteredData = filteredData.filter(
-            (startup) => startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
-        );
-    }
-
-    res.json(filteredData);
+app.use((req, res) => {
+    res.status(404).json({ message: 'Endpoint not found. Please check the API documentation.' });
 });
 
-app.get('/api/:field/:term', (req, res) => {
-    const { field, term } = req.params;
-
-    const allowedFields = ['country', 'continent', 'industry'];
-
-    if (!allowedFields.includes(field)) {
-        return res.status(400).json({
-            message: "Search field not allowed. Please use only 'country', 'continent', 'industry'",
-        });
-    }
-
-    const filteredData = startups.filter(
-        (startup) => startup[field].toLowerCase() === term.toLowerCase()
-    );
-
-    res.json(filteredData);
-});
-
-app.listen(PORT, () => console.log(`server connected to port ${PORT}`));
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
