3342367f420844218721af23bb5ccf0474beaa64
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-15 17:18:32 -0700

    basic setup

--- Express/server.js
+++ Express/server.js
@@ -1,10 +1,18 @@
-import http from 'node:http';
+// import http from 'node:http';
 
-const server = http.createServer((req, res) => {
-    console.log('server accessed');
-    res.end('<html><body>Hello Rodion</body></html>');
-});
+// const server = http.createServer((req, res) => {
+//     console.log('server accessed');
+//     res.end('<html><body>Hello Rodion</body></html>');
+// });
 
-server.listen(8000, () => console.log('listening 8000'));
+// server.listen(8000, () => console.log('listening 8000'));
 
-console.log('Hello');
+// console.log('Hello');
+
+import express from 'express';
+
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server connected to port ${PORT}`));
