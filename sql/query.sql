7d8804a156f1c644cf44cda2455faaa0df8d21f3
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-18 18:37:58 -0700

    challenges

--- sql/query.sql
+++ sql/query.sql
@@ -89,11 +89,31 @@
 		and the car has not been sold
 */
 
-SELECT brand, model, color FROM cars
-    WHERE color = 'red'
-    AND brand != 'Ferrari'
-    AND sold IS FALSE;
+-- SELECT brand, model, color FROM cars
+--     WHERE color LIKE '%red%'
+--     AND brand != 'Ferrari'
+--     AND sold IS FALSE;
 
+/*
+	Select brand, model, and color from cars
+		where the color is not red, blue, or white
+		and the brand is none of: Aston Martin, Bentley or Jaguar
+		and sold is false
+*/
+-- SELECT brand, model, color FROM cars
+--     WHERE color NOT IN('red', 'blue', 'white')
+--     AND brand NOT IN('Aston Martin', 'Bentley', 'Jaguar')
+--     AND sold IS FALSE;
+/*
+	Select brand, model, year, sold from cars
+		where the brand is 'Dodge' and year is in the 60s
+		or the brand is either 'Ford' or 'Triumph' and the car is from the 70s
+		only select cars where sold is not true
+*/
+-- SELECT brand, model, year, sold FROM cars
+--   WHERE ((brand = 'Dodge' AND year BETWEEN 1960 AND 1969)
+--   OR (brand IN ('Ford', 'Triumph') AND year BETWEEN 1970 AND 1979))
+--   AND SOLD IS NOT TRUE;
 
 
 
