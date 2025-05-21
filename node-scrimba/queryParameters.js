e76657b66f5f20361cd4a6da63c1de22fa4c5f5f
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-05-21 16:36:37 -0700

    finishing up

--- node-scrimba/queryParameters.js
+++ node-scrimba/queryParameters.js
@@ -1,3 +1,15 @@
+// import http from 'node:http';
+
+// const server = http.createServer((req, res) => {
+//     const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+//     const queryObj = Object.fromEntries(urlObj.searchParams);
+
+//     console.log(queryObj);
+// });
+
+// server.listen(8000, () => console.log('Server listening on port 8000'));
+
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
@@ -8,4 +20,6 @@ const server = http.createServer((req, res) => {
     console.log(queryObj);
 });
 
-server.listen(8000, () => console.log('Server listening on port 8000'));
+server.listen(8000, () => {
+    console.log('Server is listening 8000');
+});
